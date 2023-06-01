5cf50680cc5c850b65490773f34024b07a796470
chung-d <101185104+chung-d@users.noreply.github.com>
2023-06-01 15:22:12 +0900

    Update FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -2,6 +2,20 @@
 
 print("Hello World!")
 print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
 
 for i in range(30): # updated by muhanjeju
     print("Hello Jeju National University! - in cjbranch")
